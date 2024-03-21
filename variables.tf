6581ec3d541ddbc74c9aa7e18c788ad38125d1c8
stalejohnsen <stalejohnsen>
2024-03-21 09:03:18 +0100

    Add variables.tf [skip ci]

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "subscription_id" {
+  description = "Existing subscription Id for demo purposes"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Tenant Id for Entra ID provider"
+  type        = string
+}
